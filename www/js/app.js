8fd82b6eff8f9569f7aab222244ec20db980662a
atrox3d <atrox3d@gmail.com>
2016-06-04 16:17:43 +0200

    chapter 5: add task modal

--- www/js/app.js
+++ www/js/app.js
@@ -4,15 +4,32 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('todo', ['ionic'])
-.controller('TodoCtrl', function ($scope) {
-  $scope.tasks = [
-    {title : "task1"},
-    {title : "task2"},
-    {title : "task3"},
-    {title : "task4"},
-    {title : "task5"},
-  ];
-})
+.controller('TodoCtrl', function ($scope, $ionicModal) {
+  $scope.tasks = [];
+
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal=modal;
+  }, {
+    scope: $scope,
+    animation: 'sile-in-up'
+  });
+
+  $scope.createTask = function(task) {
+    console.log('createTask');
+    $scope.tasks.push({ title: task.title});
+    $scope.taskModal.hide();
+    task.title="";
+  }
+
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  }
+
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  }
+
+});
 
 // .run(function($ionicPlatform) {
 //   $ionicPlatform.ready(function() {
